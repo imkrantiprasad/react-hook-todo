b3854627d9183387a11e2ffbdc036143a25ac231
krantiprasad12 <57789517+krantiprasad12@users.noreply.github.com>
2020-09-14 21:52:30 +0530

    Updates

---
+++ precache-manifest.a07b28d3b2ba5e8818f2ef17f2afb080.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a703e90f095da2529deef7de63526e9d",
+    "url": "/react-hook-todo/index.html"
+  },
+  {
+    "revision": "7cf67726de6f1c89a1ca",
+    "url": "/react-hook-todo/static/css/main.17b17d05.chunk.css"
+  },
+  {
+    "revision": "2b7ac48767d262f10370",
+    "url": "/react-hook-todo/static/js/2.e3728ba3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-hook-todo/static/js/2.e3728ba3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7cf67726de6f1c89a1ca",
+    "url": "/react-hook-todo/static/js/main.caa3ffbb.chunk.js"
+  },
+  {
+    "revision": "1452ecd2cb673511192b",
+    "url": "/react-hook-todo/static/js/runtime-main.fb13a24c.js"
+  }
+]);
