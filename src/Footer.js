e72e55cfa3f7e38bdb6353d1b5df2a536b26b7b7
krantiprasad12 <57789517+krantiprasad12@users.noreply.github.com>
2020-09-15 21:12:53 +0530

    updated styles

--- src/Footer.js
+++ src/Footer.js
@@ -4,6 +4,8 @@ export default function Footer() {
     return (
         <div className="footer">
             <a href="https://github.com/imkrantiprasad/react-hook-todo">GitHub link</a>
+            <br />
+            Made with <span className="heart">&hearts;</span> by <a href="https://www.linkedin.com/in/kranti-prasad-690034131/">Kranti</a>
         </div>
     );
 }
