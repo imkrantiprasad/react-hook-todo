df7da030c46077ae8c7ca883ce9937d96ef04dd4
rpollock03 <49661708+rpollock03@users.noreply.github.com>
2020-10-30 16:34:04 +0000

    add delete button animation

--- src/Todo.js
+++ src/Todo.js
@@ -2,15 +2,14 @@ import React from 'react';
 
 export default function Todo({ todo, index, completeTodo, removeTodo }) {
     return (
-        <div
+        <div onClick={() => completeTodo(index)}
             className="todo"
             style={{ textDecoration: todo.isCompleted ? "line-through" : "" }}
         >
             {todo.text}
 
             <div>
-                <button className="btn mark-btn" onClick={() => completeTodo(index)}>{todo.isCompleted ? `Unmark` : `Mark`}</button>
-                <button className="btn delete-btn" onClick={() => removeTodo(index)}>x</button>
+                <button className="btn delete-btn" onClick={(event) => { event.stopPropagation(); removeTodo(index) }}>x</button>
             </div>
         </div>
     );
