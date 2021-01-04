162b0ff06287221aff717d1dd31730916ff66928
Kranti Prasad <krantiprasad07@gmail.com>
2021-01-04 16:12:28 +0530

    netlify deploy

--- src/Todo.js
+++ src/Todo.js
@@ -1,17 +1,37 @@
-import React from 'react';
+import React from "react";
 
-export default function Todo({ todo, index, completeTodo, removeTodo, editTodo }) {
-    return (
-        <div onClick={() => completeTodo(index)}
-            className="todo"
-            style={{ textDecoration: todo.isCompleted ? "line-through" : "" }}
-        >
-            {todo.text}
+export default function Todo({
+	todo,
+	index,
+	completeTodo,
+	removeTodo,
+	editTodo,
+}) {
+	return (
+		<div
+			onClick={() => completeTodo(index)}
+			className="todo"
+			style={{ textDecoration: todo.isCompleted ? "line-through" : "" }}>
+			{todo.text}
 
-            <div>
-                <button className="btn edit-btn" onClick={(event) => { event.stopPropagation(); editTodo(index) }}>Edit </button>
-                <button className="btn delete-btn" onClick={(event) => { event.stopPropagation(); removeTodo(index) }}>x</button>
-            </div>
-        </div>
-    );
+			<div>
+				<button
+					className="btn edit-btn"
+					onClick={(event) => {
+						event.stopPropagation();
+						editTodo(index);
+					}}>
+					Edit{" "}
+				</button>
+				<button
+					className="btn delete-btn"
+					onClick={(event) => {
+						event.stopPropagation();
+						removeTodo(index);
+					}}>
+					x
+				</button>
+			</div>
+		</div>
+	);
 }
